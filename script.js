9ea590de743c64200394259006981ef4416dd5cc
Mɪʀᴀɴᴅᴀ乡 Gustavo <gustavo.aguia@eaportal.org>
2024-08-14 21:17:49 +0000

    att agora temos js

---
+++ script.js
@@ -0,0 +1,16 @@
+const respostaAPI = [
+    { "id": 1, "descrição":"Varrer a sala"}, // "Objeto" (JSON)
+    { "id": 2, "descrição":"Revisar a aula de BD"},
+    { "id": 3, "descrição":"Lavar a louça"},
+]; // Array - Matriz
+
+function init(){
+    carregarLista();
+}
+
+function carregarLista() {
+    let idx = -1;
+    while(respostaAPI.lenght > 0 && ++idx < respostaAPI.length){
+
+    }
+}
